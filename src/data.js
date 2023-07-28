527c8ae088e3c80a466cf0b441805f699172e103
Mariya-go <mariya.golovko@gmail.com>
2023-07-28 11:54:30 +0200

    add data.js

---
+++ src/data.js
@@ -0,0 +1,23 @@
+// add initial tasks
+const data = {
+    tasks: [
+        {
+            id: 1,
+            text: 'get a smile'
+        },
+        {
+            id: 2,
+            text: 'do something good for you'
+        },
+        {
+            id: 3,
+            text: 'take a rest, life is not only job'
+        }
+    ]
+}
+
+// add dom elements
+const dom = {
+    input: document.getElementById('grocery'),
+    submitBtn: document.getElementById("submit-btn")
+}
